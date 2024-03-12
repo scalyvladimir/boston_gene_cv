2a7920d78e049ad42752b0885812bb9b15336690
Vladimir Chernyy <ghost23rus@gmail.com>
2024-03-13 01:39:35 +0300

    day 1

---
+++ data.py
@@ -0,0 +1,73 @@
+import lightning as pl
+from torch.utils.data import DataLoader, Subset, WeightedRandomSampler
+from sklearn.model_selection import train_test_split
+from torchvision import transforms as TT
+from torchvision import datasets
+import torch
+import os
+
+
+class BGDataModule(pl.LightningDataModule):
+    def __init__(self, data_dir: str = "./"):
+        super().__init__()
+
+        self.data_dir = data_dir
+        assert os.path.isdir(data_dir)
+
+        self.dataset = datasets.ImageFolder(
+            root=self.data_dir,
+            transform=TT.Compose([
+                TT.Resize((128, 128)),
+                TT.ToTensor(),
+                TT.Normalize(
+                    mean=[0.485, 0.456, 0.406],
+                    std=[0.229, 0.224, 0.225]
+                )
+            ])
+        )
+
+        # self.transform = None #transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))])
+        
+        self.seed = 0xC0FFEE
+
+        self.train_idx, self.val_idx = train_test_split(
+            torch.arange(len(self.dataset), dtype=torch.int),
+            test_size=0.2,
+            random_state=self.seed,
+            stratify=self.dataset.targets
+        )
+
+    def setup(self, stage: str):
+        if stage == 'fit':
+            self.train_dataset = Subset(self.dataset, self.train_idx)
+            self.val_dataset = Subset(self.dataset, self.val_idx)
+
+            t_tgts = torch.tensor(self.dataset.targets)
+
+            _, class_cnts = torch.unique(t_tgts[self.train_idx], return_counts=True)
+    
+            sample_weights = torch.zeros(len(t_tgts[self.train_idx]))
+            
+            for i, label_id in enumerate(self.train_idx):
+                sample_weights[i] = 1. / class_cnts[t_tgts[label_id]]
+            
+            N = int(max(class_cnts) * len(class_cnts))
+            
+            self.train_sampler = WeightedRandomSampler(sample_weights, num_samples=N//2)
+
+        if stage == "test":
+            self.val_dataset = Subset(self.dataset, self.val_idx)
+
+    def train_dataloader(self):
+        return DataLoader(
+            dataset=self.train_dataset,
+            batch_size=32,
+            pin_memory=True,
+            sampler=self.train_sampler
+        )
+
+    def val_dataloader(self):
+        return DataLoader(self.val_dataset, batch_size=32)
+
+    def test_dataloader(self):
+        return DataLoader(self.val_dataset, batch_size=32)
---
+++ models.py
@@ -0,0 +1,86 @@
+import pytorch_lightning as pl
+from torch.optim import Adam, AdamW
+from torch.optim.lr_scheduler import CosineAnnealingLR
+from torchmetrics import F1Score
+import torchvision
+import torch.nn as nn
+import torch
+
+class ClassificationNet(pl.LightningModule):
+    
+    def __init__(self, num_classes, freeze_ratio, n_epochs, class_weights=None, model=None):
+        super().__init__()
+        
+        self.n_epochs = n_epochs
+        
+        if class_weights is not None:
+            self.class_weights = torch.Tensor(class_weights).to(torch.device('cuda:0' if torch.cuda.is_available else 'cpu'))
+        else:
+            self.class_weights = None
+
+        if model is not None:
+            self.model = model
+        else:
+            self.model = torchvision.models.resnet50(pretrained=True)
+            self.model.fc = nn.Linear(in_features=2048, out_features=num_classes, bias=True)
+        
+        params = list(self.model.parameters())
+        params_t = int(len(params) * freeze_ratio)
+
+        for param in params[:params_t]:
+            param.requires_grad = False
+        
+        self.sm = nn.Softmax(dim=1)
+        
+        self.train_f1 = F1Score(average='macro', num_classes=num_classes, mdmc_average='global')
+        self.val_f1 = F1Score(average='macro', num_classes=num_classes, mdmc_average='global')
+    
+    def forward(self, x):
+        
+        logits = self.model(x)
+        
+        return logits
+    
+    def configure_optimizers(self):
+        optimizer = AdamW(self.model.parameters(), lr=1e-3)
+        scheduler = CosineAnnealingLR(optimizer, T_max=self.n_epochs, eta_min=1e-4)
+        return [optimizer], [scheduler]
+    
+    def training_step(self, train_batch, batch_idx):
+        x, y = train_batch
+        
+        logits = self.forward(x)
+        
+        preds = self.sm(logits)
+        
+        self.train_f1(preds=preds, target=y)
+        self.log('train_f1', self.train_f1, on_step=True, on_epoch=False)
+        
+        loss = nn.functional.cross_entropy(
+            preds,
+            y
+        )
+        self.log('train_loss', loss)
+        
+        return loss
+
+    def validation_step(self, val_batch, batch_idx):
+        x, y = val_batch
+        
+        logits = self.forward(x)
+        
+        preds = self.sm(logits)
+                
+        self.val_f1(preds=preds, target=y)
+        self.log('val_f1', self.val_f1, on_step=False, on_epoch=True)
+        
+        loss = nn.functional.cross_entropy(
+            preds,
+            y
+        )
+        self.log('val_loss', loss)
+        
+        return loss
+        
+    def predict_step(self, batch, batch_idx, dataloader_idx=0):
+        return torch.argmax(self.sm(self(batch)), dim=1)
